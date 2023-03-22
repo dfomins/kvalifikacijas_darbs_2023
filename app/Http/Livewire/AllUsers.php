0ef56afe03c4168f939036c1fcb158e5e6c99954
Daniels Fomins <danielsfomins92@gmail.com>
2023-03-22 16:03:16 +0200

    all user page

--- app/Http/Livewire/AllUsers.php
+++ app/Http/Livewire/AllUsers.php
@@ -5,9 +5,56 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 use App\Models\User;
+use App\Models\Role;
+
+use Validator;
 
 class AllUsers extends Component
 {
+
+    public $editIndex = null;
+
+    public $fname;
+
+    public function changeRole(User $user, $role_id)
+    {
+
+        Validator::make(['role_id' => $role_id], [
+            'role_id' => 'required|in:1,2,3',
+        ])->validate();
+
+        $user->update(['role_id' => $role_id]);
+    }
+
+    public function remove($user_id)
+    {
+        $user = User::find($user_id);
+        $user->delete();
+    }
+
+    public function edit(User $user)
+    {
+        $this->fname = $user->fname;
+        $this->editIndex = $user->id;
+    }
+
+    public function save(User $user)
+    {
+
+        $this->validate([
+            'fname' => ['required', 'string', 'max:50'],
+            // 'lname' => ['required', 'string', 'max:50'],
+            // 'email' => ['required', 'string', 'email', 'max:100', 'unique:users'],
+        ]);
+
+        $user->update(['fname' => $this->fname]);
+        $this->editIndex = null;
+    }
+
+    public function cancel()
+    {
+        $this->editIndex = null;
+    }
 
     public function render()
     {
--- resources/views/livewire/all-users.blade.php
+++ resources/views/livewire/all-users.blade.php
@@ -21,16 +21,52 @@
                 @foreach ($users as $user)
                     <tr class="color-1 h-[50px]">
                         <td class="w-[5%] border-r border-t text-center">{{ $user->id }}</td>
-                        <td class="w-[20%] border-r border-t pl-[10px]">{{ $user->fname }}</td>
+                        <td class="w-[20%] border-r border-t pl-[10px]">
+
+                            @if ($user->id == $editIndex)
+                                <input class="text-black outline-0" type="text" wire:model.defer="fname">
+                            @else
+                                {{ $user->fname }}
+                            @endif
+
+                        </td>
                         <td class="w-[20%] border-r border-t pl-[10px]">{{ $user->lname }}</td>
                         <td class="w-[35%] border-r border-t pl-[10px]">{{ $user->email }}</td>
-                        <td class="border-r border-t pl-[10px]">{{ $user->role->name }}</td>
-                        <td class="border-r border-t text-center">
+                        <td class="border-r border-t text-center text-black">
                             @if ($user->id != Auth::user()->id)
-                                <i class="fa-regular fa-pen-to-square mr-[5px] text-[20px]"></i>
-                                <i class="fa-regular fa-trash-can ml-[5px] text-[20px]"></i>
+                                <select class="cursor-pointer outline-0"
+                                    wire:change="changeRole({{ $user->id }}, $event.target.value)">
+                                    <option value="1" {{ $user->role_id == '1' ? 'selected' : '' }}>Vadītājs
+                                    </option>
+                                    <option value="2" {{ $user->role_id == '2' ? 'selected' : '' }}>Brigadieris
+                                    </option>
+                                    <option value="3" {{ $user->role_id == '3' ? 'selected' : '' }}>Darbinieks
+                                    </option>
+                                </select>
                             @else
-                                <i class="fa-regular fa-pen-to-square mr-[5px] text-[20px]"></i>
+                                <div class="text-white">
+                                    {{ $user->role->name }}
+                                </div>
+                            @endif
+                        </td>
+                        <td class="border-r border-t text-center">
+                            @if ($user->id != $editIndex)
+                                @if ($user->id != Auth::user()->id)
+                                    <i class="fa-regular fa-pen-to-square mr-[5px] cursor-pointer text-[20px]"
+                                        wire:click="edit({{ $user->id }})"></i>
+                                    <i class="fa-regular fa-trash-can ml-[5px] cursor-pointer text-[20px]"
+                                        wire:click="remove({{ $user->id }})"></i>
+                                @else
+                                    <i class="fa-regular fa-pen-to-square mr-[5px] cursor-pointer text-[20px]"
+                                        wire:click="edit({{ $user->id }})"></i>
+                                @endif
+                            @else
+                                <button wire:click.prevent="save({{ $user->id }})"><i
+                                        class="fa-solid fa-check mr-[5px] cursor-pointer text-[20px]"></i>
+                                </button>
+                                <button wire:click.prevent="cancel()">
+                                    <i class="fa-solid fa-xmark ml-[5px] text-[20px]"></i>
+                                </button>
                             @endif
                         </td>
                     </tr>
