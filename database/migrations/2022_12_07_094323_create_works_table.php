667cb348389b4c58de3321018838ccd8c845835b
Daniels Fomins <danielsfomins92@gmail.com>
2023-05-16 15:07:29 +0300

    samainits migration limits atskaites stundam

--- database/migrations/2022_12_07_094323_create_works_table.php
+++ database/migrations/2022_12_07_094323_create_works_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->date('date');
-            $table->char('hours');
+            $table->char('hours', 1);
             $table->timestamps();
         });
 
