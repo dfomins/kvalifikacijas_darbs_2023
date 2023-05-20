6a88163ee5a7250a1659df5cf5609fc678676f8a
Daniels Fomins <danielsfomins92@gmail.com>
2023-05-20 12:10:03 +0300

    favicon path fixed

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="storage/images/logo/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="storage/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400;500;600;700;800;900&display=swap"
